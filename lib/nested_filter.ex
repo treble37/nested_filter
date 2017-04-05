a6b9b55e49adb88cea216c420c596a9ca237f32c
Steve Cohen <steve@pinterest.com>
2017-04-04 18:12:44 -0700

    Switched to predicate oriented version

--- lib/nested_filter.ex
+++ lib/nested_filter.ex
@@ -3,26 +3,30 @@ defmodule NestedFilter do
   Documentation for NestedFilter.
   """
 
+  def drop_by(map, predicate) when is_map(map) do
+    map
+    |> Enum.reduce(%{},
+    fn ({key, val}, acc) ->
+      cleaned_val = drop_by(val, predicate)
+      if predicate.(key, cleaned_val) do
+        acc
+      else
+        Map.put(acc, key, cleaned_val)
+      end
+    end)
+  end
+
+  def drop_by(elem, _) do
+    elem
+  end
+
   @doc """
   Take a (nested) map and filter out any keys with specified values in the
   values_to_reject list.
   """
   @spec drop_by_value(map :: map(), values_to_reject :: list()) :: map()
   def drop_by_value(map, values_to_reject) when is_map(map) do
-    map
-    |> Enum.reduce(%{}, fn
-      ({key, val}, acc) ->
-        cleaned_val = drop_by_value(val, values_to_reject)
-        if cleaned_val in values_to_reject do
-          acc
-        else
-          Map.put(acc, key, cleaned_val)
-        end
-    end)
-  end
-
-  def drop_by_value(not_map, _) do
-    not_map
+    drop_by(map, fn (_, val) -> val in values_to_reject end)
   end
 
   @doc """
@@ -31,19 +35,6 @@ defmodule NestedFilter do
   """
   @spec drop_by_key(map :: map(), keys_to_reject :: list()) :: map()
   def drop_by_key(map, keys_to_reject) when is_map(map) do
-    map
-    |> Enum.reduce(%{},
-    fn
-      ({key, val}, acc)  ->
-        if key in keys_to_reject do
-          acc
-        else
-          Map.put(acc, key, drop_by_key(val, keys_to_reject))
-        end
-    end)
-  end
-
-  def drop_by_key(not_map, _) do
-    not_map
+    drop_by(map, fn(key, _) -> key in keys_to_reject end)
   end
 end
