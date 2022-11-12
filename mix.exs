c13bdc9f3abb7fc91e340947a89958aaf19a6c6f
Bruce P <treble37@users.noreply.github.com>
2022-11-11 20:11:49 -0800

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -50,10 +50,10 @@ defmodule NestedFilter.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, ">= 0.25.3", only: :dev},
-      {:excoveralls, "~> 0.14.3", only: :test},
+      {:ex_doc, ">= 0.29.0", only: :dev},
+      {:excoveralls, "~> 0.15.0", only: :test},
       {:inch_ex, "~> 2.0.0", only: :docs},
-      {:credo, "~> 1.5.6", only: [:dev, :test]}
+      {:credo, "~> 1.6.7", only: [:dev, :test]}
     ]
   end
 end
