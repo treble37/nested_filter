93a2a0bc697415746e8f804b53ef7558b71f7fa4
Bruce P <treble37@users.noreply.github.com>
2023-10-04 23:39:32 -0700

    Update deps
    - Remove inch_ex

--- mix.exs
+++ mix.exs
@@ -51,9 +51,8 @@ defmodule NestedFilter.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.29.0", only: :dev},
-      {:excoveralls, "~> 0.15.0", only: :test},
-      {:inch_ex, "~> 2.0.0", only: :docs},
-      {:credo, "~> 1.6.7", only: [:dev, :test]}
+      {:excoveralls, "~> 0.16.1", only: :test},
+      {:credo, "~> 1.7.0", only: [:dev, :test]}
     ]
   end
 end
